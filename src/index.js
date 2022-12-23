458bfbeb38615734e3650fa9bff9a01262c2ae99
Artem <artem4rolov@gmail.com>
2022-12-23 14:25:38 +0300

    сверстана фронтенд часть блога, добавлен react-router

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import CssBaseline from "@mui/material/CssBaseline";
+import { BrowserRouter } from "react-router-dom";
 
 import "./index.scss";
 import { ThemeProvider } from "@mui/material";
@@ -10,10 +11,12 @@ import { theme } from "./theme";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <React.StrictMode>
-    <CssBaseline />
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
-  </React.StrictMode>
+  <BrowserRouter>
+    <React.StrictMode>
+      <CssBaseline />
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </React.StrictMode>
+  </BrowserRouter>
 );
