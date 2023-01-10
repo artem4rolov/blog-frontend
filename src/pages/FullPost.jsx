ddc724e37a4c0517bbc868f89fc2aa25dd05a7ff
Artem <artem4rolov@gmail.com>
2023-01-10 23:35:34 +0300

    корректное отображение статей на главной странице при удалении статьи

--- src/pages/FullPost.jsx
+++ src/pages/FullPost.jsx
@@ -38,7 +38,7 @@ export const FullPost = () => {
         key={data._id}
         id={data._id}
         title={data.title}
-        imageUrl={`http://localhost:4444${data.imageUrl}`}
+        imageUrl={data.imageUrl ? `http://localhost:4444${data.imageUrl}` : ""}
         user={data.user}
         createdAt={data.createdAt}
         viewsCount={data.viewsCount}
--- src/redux/slices/posts.js
+++ src/redux/slices/posts.js
@@ -13,6 +13,14 @@ export const fetchTags = createAsyncThunk("posts/fetchTags", async () => {
   return data;
 });
 
+// удаление поста
+export const fetchRemovePost = createAsyncThunk(
+  "posts/fetchRemovePost",
+  async (id) => {
+    await axios.delete(`/posts/${id}`);
+  }
+);
+
 const initialState = {
   posts: {
     items: [],
@@ -29,6 +37,7 @@ const postsSlice = createSlice({
   initialState,
   reducers: {},
   extraReducers: {
+    // ПОЛУЧЕНИЕ СТАТЬИ
     // отлавливаем состояния загрузки постов с бэка
     // в то время, когда происходит загрузка
     [fetchPosts.pending]: (state) => {
@@ -49,20 +58,25 @@ const postsSlice = createSlice({
       state.posts.items = [];
       state.posts.status = "error";
     },
-    // Теги
+    // ПОЛУЧЕНИЕ ТЕГОВ
     [fetchTags.pending]: (state) => {
       state.tags.items = [];
       state.tags.status = "loading";
     },
-
     [fetchTags.fulfilled]: (state, action) => {
       state.tags.items = action.payload;
       state.tags.status = "loaded";
     },
-
     [fetchTags.rejected]: (state) => {
       state.tags.items = [];
       state.tags.status = "error";
+    },
+    // УДАЛЕНИЕ СТАТЬИ
+    [fetchRemovePost.pending]: (state, action) => {
+      // удаляем статью из стейта
+      state.posts.items = state.posts.items.filter(
+        (obj) => obj._id !== action.meta.arg
+      );
     },
   },
 });
